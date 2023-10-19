0901b9ca6311275d2671e71431e232d08827d28e
Anushka Karhadkar <124391010+Anush0108@users.noreply.github.com>
2023-10-19 22:25:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-// App.js
 import React from 'react';
+import { BrowserRouter as Router, Route,Routes} from 'react-router-dom';
+import Profile from './Profile';
+import Contact from './Contact';
+import "./App.css"
 
-import Home from './Home';
-
-function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          
+          <Routes>
+            <Route path='/' element={<Profile />} />
+            <Route path='/contact' element={<Contact />} />
+            </Routes>
+       
+          
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
